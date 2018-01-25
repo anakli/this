fe455f40671443ffcdcf69705ac3ab37381a00da
Ana Klimovic <ana.klimov@gmail.com>
2018-01-25 03:25:21 +0000

    runlog parsing for gg with crail

---
+++ logs/fetch-crail-gg-runlogs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import crail
+import time
+
+datadir = sys.argv[1]
+#netstat_dir= os.path.join(datadir, "netstats")
+
+if not os.path.exists(datadir):
+    os.makedirs(datadir)
+    #os.makedirs(netstat_dir)
+
+#r = redis.StrictRedis(host=os.environ['REDIS_HOSTADDR'], port=6379, db=0)
+p = crail.launch_dispatcher("/home/ubuntu/crail/crail-deployment/crail-1.0")
+time.sleep(10)
+socket = crail.connect()
+ticket = 1000
+
+
+
+
+#FIXME: get list of reqids
+#runlog_keys = r.keys(pattern="video-lambda-logs*")
+with open('crail-runlog-reqids.txt', 'r') as idfile:
+  for line in idfile:
+    reqid = line.replace('\n', '')
+    logfile = "/runlogs/" + reqid
+    crail.get(socket, logfile, datadir + "/" + reqid, ticket) 
+    #netstatFile = "/video-lambda-logs/netstats-" + reqid
+    #crail.get(socket, logfile, netstat_dir + "/" + reqid, ticket) 
+
+crail.close(socket, ticket, p)
---
+++ logs/plot-crail-gg-breakdown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+#~/crail/crail-deployment/crail-1.0/bin/crail fs -ls /runlogs | grep -v "lambda" | grep -v "Found" | awk '{ print $8 }' | sed -e 's/\/runlogs\///' > ~/this/logs/crail-runlog-reqids.txt
+~/crail/crail-deployment/crail-1.0/bin/crail fs -ls /runlogs | grep "runlogs" | awk '{ print $8 }' | sed -e 's/\/runlogs\///' > ~/this/logs/crail-runlog-reqids.txt
+./fetch-crail-gg-runlogs.py $1
+./parse-runlogs.py $1 > timing_data
+gnuplot timingbase_gg.gnu
+cp timing_data graphs/${1}.txt
+cp graph.svg graphs/${1}.svg
