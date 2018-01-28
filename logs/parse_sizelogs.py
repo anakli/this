20ddc3c0369870ad7c3b5ece6b276878e07d21fa
Ana Klimovic <ana.klimov@gmail.com>
2018-01-28 08:52:11 +0000

    sizelog cdf plot

--- logs/parse_sizelogs.py
+++ logs/parse_sizelogs.py
@@ -5,16 +5,64 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({'font.size': 20})
+
 datadir = sys.argv[1]
+datadir = os.path.join(datadir, 'sizelogs')
 
 data_points = []
 headers = []
 
 infiles = {}
-
+file_sizes = []
 for logfile in os.listdir(datadir):
     path = os.path.join(datadir, logfile)
 
+    if logfile in "netstats":
+	continue
+    print "*******" + path + "********"
+    with open(path, "r") as log:
+        data = eval(log.read())
+        point =  [] #[data['started']]
+        name  = []
+        name += [x[0] for x in data['sizelog']]
+        point += [x[1] for x in data['sizelog']]
+        data_points += [point]
+        
+        for x in range(0,len(point)):
+            infile_shorthash = name[x][name[x].find("'")+1:name[x].find("'")]
+            infile_size = int(point[x])
+            infiles[infile_shorthash] = infile_size
+            print infile_shorthash + " --> " + " %d" % infile_size
+            file_sizes.append(infile_size)
+
+        #if not headers:
+        #    headers += ["start"]
+        #    headers += [x[0] for x in data['timelog']]
+
+data_points.sort(key=lambda x: x[1])
+
+fig, ax = plt.subplots(figsize=(8, 4))
+x = infiles.values() 
+x = file_sizes
+print x
+x[:] = [i / 1024 for i in x] #KB
+n_bins = 8000000 #len(x)
+print len(x)
+n, bins, patches = ax.hist(x, n_bins, normed=1, histtype='step',
+                           cumulative=True, label='video analytics')
+
+
+
+# gg data : mosh-ana-lambda100-sizelogs
+datadir = sys.argv[2]
+
+file_sizes = []
+for logfile in os.listdir(datadir):
+    path = os.path.join(datadir, logfile)
+
+    if logfile in "netstats":
+	continue
     print "*******" + path + "********"
     with open(path, "r") as log:
         data = eval(log.read())
@@ -29,25 +77,23 @@ for logfile in os.listdir(datadir):
             infile_size = int(point[x])
             infiles[infile_shorthash] = infile_size
             print infile_shorthash + " --> " + " %d" % infile_size
+            file_sizes.append(infile_size)
 
-        #if not headers:
-        #    headers += ["start"]
-        #    headers += [x[0] for x in data['timelog']]
-
-data_points.sort(key=lambda x: x[1])
-
-fig, ax = plt.subplots(figsize=(8, 4))
-x = infiles.values() 
+#x = [ int(i) for i in file_sizes if i.isdigit()] 
+#x = filter( lambda i: i > 0, x)
+x = file_sizes
 x[:] = [i / 1024 for i in x] #KB
-n_bins = len(x)
-print len(x)
+#print "infile size KBs is: ", sorted(x)
+n_bins = 8000000
 n, bins, patches = ax.hist(x, n_bins, normed=1, histtype='step',
                            cumulative=True, label='gg-mosh')
-ax.legend(loc='right')
-ax.set_title('Data file size distribution')
+patches[0].set_xy(patches[0].get_xy()[:-1])
+
+ax.legend(loc='lower right')
+#ax.set_title('Data file size distribution')
 ax.set_xscale('log') #,nonposx='clip')
-ax.set_xlabel('Input File size (KB)')
-ax.set_xlim(0,max(x))
+ax.set_xlabel('I/O size (KB)')
+ax.set_xlim(1,max(x))
 ax.set_ylim(0,1)
 ax.set_ylabel('CDF')
 
